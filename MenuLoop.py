4c0025cdb3ecf3279d1ee29346758e59c4e017ba
LeoGori <leonardogori@hotmail.it>
2019-09-03 00:16:44 +0200

    Add files via upload

--- MenuLoop.py
+++ MenuLoop.py
@@ -4,6 +4,7 @@ import random
 import os
 from Screen import Menu
 
+
 class MenuLoop:
 
     def __init__(self):
@@ -12,31 +13,28 @@ class MenuLoop:
     def generate_screen(self):
 
         pygame.init()
+        # display = pygame.display.set_mode((1000, 600))
+        # pygame.display.set_caption("Puzzle_Game")
 
-        display = pygame.display.set_mode((800, 600))
-        pygame.display.set_caption("Puzzle")
-        
         while True:
-            pygame.draw.rect(display, (0, 0, 0), (0, 0, 800, 600))
 
-            pygame.time.delay(100)            
+            pygame.time.delay(100)
 
-            event = pygame.event.wait()
+            event = pygame.event.poll()
 
             self.screen.get_activities(event)
-            
+
             if self.screen.get_state():
                 self.screen = self.screen.get_next_state()
 
             self.screen.draw()
 
 
-
 def main():
-
     ml = MenuLoop()
 
     ml.generate_screen()
 
+
 if __name__ == "__main__":
-        main()
+    main()
--- Screen.py
+++ Screen.py
@@ -4,19 +4,27 @@ import sys
 import os
 import random
 from Tile import Tile
+from stopwatch import stopwatch
 
 white = (255, 255, 255)
 green = (0, 255, 0)
 blue = (0, 0, 128)
+red = (255, 0, 0)
+
 
 class GraphicState(ABC):
     def __init__(self):
+        pygame.init()
+        self.display = pygame.display.set_mode((1000, 600))
+        pygame.display.set_caption("Puzzle_Game")
         self.state_changed = False
+        self.error_message = False
+        self.time = 0
 
     def get_state(self):
         return self.state_changed
 
-    def set_state(self,state):
+    def set_state(self, state):
         self.state_changed = state
 
     def draw(self):
@@ -25,39 +33,33 @@ class GraphicState(ABC):
     def get_next_state(self):
         pass
 
-    def get_activities(self,event):
+    def get_activities(self, event):
         pass
-    
+
+
 class FinalScreen(GraphicState):
 
-    def __init__(self, state):
-        pygame.init()
+    def __init__(self):
 
         super().__init__()
-        #self.image = pygame.image.load("img/elephant.jpg")
+        # self.image = pygame.image.load("img/elephant.jpg")
         self.font = pygame.font.Font("Risorse/AlexandriaFLF.ttf", 32)
 
-        if state:
-            self.text = self.font.render('Congratulazioni! Hai vinto!', True, green, blue)
-        else:
-            self.text = self.font.render('Peccato! Andrà meglio la prossima volta!', True, green, blue)
+        self.text = self.font.render('Congratulazioni! Hai vinto!', True, green, blue)
 
         self.textRect = self.text.get_rect()
 
         # set the center of the rectangular object.
-        self.textRect.center = (800 // 2, 600 // 2)
+        self.textRect.center = (1000 // 2, 600 // 2)
 
     def draw(self):
-        display = pygame.display.set_mode((800, 600))
-        #display.blit(self.image, (0, 0))
-        display.blit(self.text, self.textRect)
+        # display.blit(self.image, (0, 0))
+        self.display.blit(self.text, self.textRect)
 
-        pygame.display.set_caption("Puzzle_Game")
         pygame.display.flip()
         pygame.display.update()
 
-    def get_activities(self,event):
-        display = pygame.display.set_mode((800, 600))
+    def get_activities(self, event):
 
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -70,27 +72,27 @@ class FinalScreen(GraphicState):
     def get_next_state(self):
         return Menu()
 
+
 class Menu(GraphicState):
 
     def __init__(self):
-        pygame.init()
 
         super().__init__()
-        self.image = pygame.image.load("Risorse/mondrian.jpg")
-        self.font = pygame.font.Font("Risorse/AlexandriaFLF.ttf", 32)
-        self.text = self.font.render('Premere invio per iniziare', True, green, blue)
+
+        self.image = pygame.image.load("Risorse/mondrian2.jpg")
+        font = pygame.font.Font("Risorse/AlexandriaFLF.ttf", 32)
+        self.text = font.render('Premere invio per iniziare', True, green, blue)
 
         self.textRect = self.text.get_rect()
 
         # set the center of the rectangular object.
-        self.textRect.center = (800 // 2, 600 // 2)
+        self.textRect.center = (1000 // 2, 600 // 2)
 
     def draw(self):
-        display = pygame.display.set_mode((800, 600))
-        display.blit(self.image, (0, 0))
-        display.blit(self.text, self.textRect)
 
-        pygame.display.set_caption("Puzzle_Game")
+        self.display.blit(self.image, (0, 0))
+        self.display.blit(self.text, self.textRect)
+
         pygame.display.flip()
         pygame.display.update()
 
@@ -100,18 +102,17 @@ class Menu(GraphicState):
     def get_activities(self, event):
 
         if event.type == pygame.QUIT:
-            print("chiudi")
             pygame.quit()
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
-                print("invio")
                 self.set_state(True)
 
 
 class Game(GraphicState):
     def __init__(self):
+
         super().__init__()
         self.FOLDER_PATH = "img"
         self.IMAGE_SIZE = (800, 600)
@@ -125,21 +126,25 @@ class Game(GraphicState):
         index = random.randrange(0, len(files))
         self.IMAGE_FILE = files[index]
 
-        image = pygame.image.load(self.FOLDER_PATH + "/" + self.IMAGE_FILE)
+        self.image = pygame.image.load(self.FOLDER_PATH + "/" + self.IMAGE_FILE)
+        self.sprite = pygame.transform.scale(self.image, (200, 150))
+
+        font = pygame.font.Font("Risorse/AlexandriaFLF.ttf", 32)
+        self.text = font.render('Ops! Posizione sbagliata!', True, red, blue)
+
+        self.textRect = self.text.get_rect()
+
+        # set the center of the rectangular object.
+        self.textRect.center = (1000 // 2, 600 // 2)
+
         self.tiles = []
 
         for c in range(self.ROWS):
             for r in range(self.COLUMNS):
-                sprite = image.subsurface(r * self.TILE_WIDTH, c * self.TILE_HEIGHT, self.TILE_WIDTH, self.TILE_HEIGHT)
+                sprite = self.image.subsurface(r * self.TILE_WIDTH, c * self.TILE_HEIGHT, self.TILE_WIDTH,
+                                               self.TILE_HEIGHT)
                 self.tiles.append(Tile(sprite))
                 self.tiles[c * self.COLUMNS + r].set_right_pos(r * self.TILE_WIDTH, c * self.TILE_HEIGHT)
-                print(c, " ", r, " ", c * self.COLUMNS + r)
-                print(self.tiles[c * self.COLUMNS + r].get_pos_x(), " ", self.tiles[c * self.COLUMNS + r].get_pos_y())
-
-        # start game
-
-        # display.blit(tiles[(0,0)], (0,0))
-        # pygame.display.flip()
 
         self.x = 0
         self.y = 0
@@ -148,17 +153,16 @@ class Game(GraphicState):
 
         self.i = len(self.tiles) - 1
 
-        self.is_the_right_pos = True
+        self.time = stopwatch.Stopwatch()
 
     def get_activities(self, event):
-        display = pygame.display.set_mode((800, 600))
 
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            pygame.draw.rect(display, (0, 0, 0), (0, 0, self.IMAGE_SIZE[0], self.IMAGE_SIZE[1]))
+
             if event.key == pygame.K_a:
                 if self.x > 0:
                     self.x = self.x - self.TILE_WIDTH
@@ -175,33 +179,38 @@ class Game(GraphicState):
                 if self.y > 0:
                     self.y = self.y - self.TILE_HEIGHT
 
-            print(self.tiles[self.i].get_pos_x(), " ", self.x, " ", self.tiles[self.i].get_pos_y(), " ", self.y)
-
             if event.key == pygame.K_RETURN:
-
-                if self.tiles[self.i].get_pos_x() != self.x or self.tiles[self.i].get_pos_y() != self.y:
-                    self.is_the_right_pos = False
-                    self.state_changed = True
 
                 if self.i == 0:
                     self.state_changed = True
 
-                print(self.i)
-
-                self.i = self.i - 1
-                self.x = 0
-                self.y = 0
+                if self.tiles[self.i].get_pos_x() != self.x or self.tiles[self.i].get_pos_y() != self.y:
+                    self.time.reset()
+                    self.time.start()
+                    self.error_message = True
+                else:
+                    self.i = self.i - 1
+                    self.x = 0
+                    self.y = 0
 
     def draw(self):
-        display = pygame.display.set_mode((800, 600))
+        pygame.draw.rect(self.display, (0, 0, 0), (0, 0, self.IMAGE_SIZE[0], self.IMAGE_SIZE[1]))
+        self.display.blit(self.sprite, (800, 225))
 
         for k in range(self.i + 1, len(self.tiles)):
-            display.blit(self.tiles[k].get_sprite(), (self.tiles[k].get_pos_x(), self.tiles[k].get_pos_y()))
+            self.display.blit(self.tiles[k].get_sprite(), (self.tiles[k].get_pos_x(), self.tiles[k].get_pos_y()))
 
-        display.blit(self.tiles[self.i].get_sprite(), (self.x, self.y))
+        pygame.draw.line(self.display, white, (800, 0), (800, 600))
+        self.display.blit(self.tiles[self.i].get_sprite(), (self.x, self.y))
+
+        if self.error_message:
+            self.display.blit(self.text, self.textRect)
+
+        if self.time.duration > 1.0:
+            self.error_message = False
 
         pygame.display.flip()
         pygame.display.update()
 
     def get_next_state(self):
-        return FinalScreen(self.is_the_right_pos)
+        return FinalScreen()
